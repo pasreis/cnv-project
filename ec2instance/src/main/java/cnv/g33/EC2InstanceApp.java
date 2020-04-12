c8322cf4069e7433e43828ee05655b8e0d6c1aa5
pasreis <pas_reis@hotmail.com>
2020-04-12 18:48:51 +0100

    Created EC2Instance module

---
+++ ec2instance/src/main/java/cnv/g33/EC2InstanceApp.java
@@ -0,0 +1,12 @@
+package cnv.g33;
+
+/**
+ * Hello world!
+ *
+ */
+public class EC2InstanceApp 
+{
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+    }
+}
