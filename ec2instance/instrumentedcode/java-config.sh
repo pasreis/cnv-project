d28692bb0dad0b900c27f2793ae15d6634509333
pasreis <pas_reis@hotmail.com>
2020-04-12 21:04:25 +0100

    Changed directory structure

---
+++ ec2instance/instrumentedcode/java-config.sh
@@ -0,0 +1,7 @@
+export JAVA_HOME=/usr/lib/jvm/java-7-oracle/
+export JAVA_ROOT=/usr/lib/jvm/java-7-oracle/
+export JDK_HOME=/usr/lib/jvm/java-7-oracle/
+export JRE_HOME=/usr/lib/jvm/java-7-oracle/jre
+export PATH=/usr/lib/jvm/java-7-oracle/bin/:$PATH
+export SDK_HOME=/usr/lib/jvm/java-7-oracle/
+export _JAVA_OPTIONS="-XX:-UseSplitVerifier "$_JAVA_OPTIONS
