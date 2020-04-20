0828c1f97d6cb649d48832f5065d0e4382744844
Pedro Reis <pas_reis@hotmail.com>
2020-04-20 10:24:21 +0100

    Changed directory structure

--- ec2instance/instrumentationcode/instrumentation/dataview/MetricsDataView.java
+++
@@ -1,5 +0,0 @@
-package instrumentation.dataview;
-
-public abstract class MetricsDataView {
-	public abstract String toString();
-}
--- ec2instance/instrumentationcode/instrumentation/observers/Observer.java
+++
@@ -1,9 +0,0 @@
-package instrumentation.observers;
-
-import instrumentation.dataview.SingleClassStaticMetricsDataView;
-import instrumentation.dataview.GlobalStaticMetricsDataView;
-
-public interface Observer {
-	public void notify(SingleClassStaticMetricsDataView singleClassStaticData);
-	public void notify(GlobalStaticMetricsDataView globalStaticData);
-}
